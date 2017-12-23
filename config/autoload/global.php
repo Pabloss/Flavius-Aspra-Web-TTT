7bd93ee44704d0140e2bccb2146d68d72fb404f6
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-12-23 21:36:16 +0100

    added zend-mvc with test

---
+++ config/autoload/global.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'config_cache_enabled' => true,
+    /* ... */
+];
---
+++ src/App/src/App/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace TicTacToe\App\src\App\Controller;
+
+use Zend\Http\Response;
+use Zend\Mvc\Controller\AbstractController;
+use Zend\Mvc\MvcEvent;
+
+class HomeController extends AbstractController
+{
+    public function onDispatch(MvcEvent $e)
+    {
+        // TODO: Implement onDispatch() method.
+    }
+
+    public function indexAction(): Response
+    {
+        return new Response();
+    }
+}
