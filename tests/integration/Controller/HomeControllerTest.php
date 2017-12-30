8c78527a9c3666498402e7b71b3bf401adcde5f4
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2017-12-30 14:49:28 +0100

    adding a test using headless browser for controller test

--- tests/integration/Controller/HomeControllerTest.php
+++ tests/integration/Controller/HomeControllerTest.php
@@ -4,11 +4,25 @@ declare(strict_types=1);
 namespace TicTacToeTest\integration\Controller;
 
 use Application\App\Controller\HomeController;
+use DMore\ChromeDriver\ChromeDriver;
 use PHPUnit\Framework\TestCase;
+use Behat\Mink\Session;
 
 class HomeControllerTest extends TestCase
 {
     const HTTP_OK_CODE = 200;
+
+    /**
+     * @var ChromeDriver
+     */
+    protected $driver;
+
+    public function setUp()
+    {
+        $chromeUrl = 'http://localhost:9222';
+        $this->driver = new ChromeDriver($chromeUrl, null, 'http://ttt.mentoring.local');
+    }
+
 
     /**
      * @test
@@ -20,5 +34,28 @@ class HomeControllerTest extends TestCase
             self::HTTP_OK_CODE,
             $controller->indexAction()->getStatusCode()
         );
+    }
+
+    /**
+     * @test
+     */
+    public function anyAction()
+    {
+        $session = $this->startSession();
+        $session->visit('http://ttt.mentoring.local');
+        $page = $session->getPage();
+        $this->assertTrue($page->hasContent('Hello'));
+    }
+
+
+    /**
+     * @return Session
+     */
+    protected function startSession()
+    {
+        $session = new Session($this->driver);
+        $session->start();
+
+        return $session;
     }
 }
